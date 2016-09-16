d668f848e075f7a40bb8a13029d2bfbf8a42dc82
Chris Drappier <chris.drappier@gmail.com>
2016-09-15 19:49:42 -0500

    hooks up refactored app to index.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { combineReducers, createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './src/react/app'
-import chessReducer from './src/react/reducers/chess_reducer'
-
-const rootReducer = combineReducers({
-  chess: chessReducer
-})
-
-const store = createStore(rootReducer)
+import App from './src/react/app_provider'
 ReactDOM.render(
-  <Provider store={store}>
     <App />
-  </Provider>
   , document.querySelector('#container'))
